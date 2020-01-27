7df308ecf64d96e59b212c37e7f0ec279de1d4e6
maria de los angeles <mariaramireeez2002@gmail.com>
2020-01-27 12:33:56 -0400

    implementacion de rutas

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,10 @@
 import {Router} from 'express'
+const path =require('path')
 
 const router=Router()
 
 router.get('/',(req,res)=>{
+    
     res.redirect('index.html')
 })
 
--- src/serve.js
+++ src/serve.js
@@ -1,19 +1,22 @@
 import express from 'express'
 import path from 'path'
-import {router} from './routes/router.js'
+import {router} from './routes/router'
 
 const app =express()
 
 const io =require('socket.io')
-const dir=path.join(__dirname,'public')
+const dir=path.join(__dirname,'../','public')
 let port=process.env.PORT || 3000
 
 
 app.use(express.urlencoded({extended:true}))
 app.use(router)
 
-
 app.use(express.static(dir))
+app.get('/video',(req,res)=>{
+    res.render('index.html')
+})
+
 app.listen(port,()=>console.log(`server at running in the port ${port}`))
 
 const server=require('http').Server(app)
