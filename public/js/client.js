e4fb69bbc0c04930bf4709caf1ed77f707d396f7
maria de los angeles <mariaramireeez2002@gmail.com>
2020-01-28 01:27:31 -0400

    mostrar por consola la velocidad Mbytes

--- public/js/client.js
+++ public/js/client.js
@@ -3,14 +3,15 @@
 
 
 
-    io().on('stream',function(image){
-        let video=document.getElementById('play')
-        //video.src+=image
-        video.src=image
-        //let logger=document.getElementById('logger')
-        //logger.text(image)
-       // console.log('show')
-       console.log( navigator.connection.downlink )
-    
-    })
+io().on('stream', function (image) {
+    let video = document.getElementById('play')
+    //video.src+=image
+    video.src = image
+    //let logger=document.getElementById('logger')
+    //logger.text(image)
+    // console.log('show')
+    //connection en mbits
+    console.log(navigator.connection.downlink)
+
+})
 
